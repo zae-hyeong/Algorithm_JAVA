73f251e7f8a84a0abdd2d77d9206ec6ab40d09c0
zae-hyeong <p0138@naver.com>
2025-07-27 22:36:42 +0900

    Merge branch 'main' of https://github.com/zae-hyeong/Algorithm_JAVA.git into main

--- baekjoon/s2_1260/Main.java
+++ baekjoon/s2_1260/Main.java
@@ -1,10 +1,48 @@
 package s2_1260;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int N, M;
+	static int[][] arr;
+	static int[] temp;
+	static ArrayList<int[]> comb = new ArrayList<>();
+	static int result = 0;
+	
+	public static void makeComb(int[] targetArr, int cnt, int start) {
+		if(cnt == M) {
+			comb.add(Arrays.copyOf(temp, M));
+			return;
+		}
+		
+		for (int i = start; i < N; i++) {
+			temp[cnt] = targetArr[i];
+			makeComb(int[] targetArr, cnt + 1, i + 1);
+		}
+		return;
 	}
 
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/test.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] comb = makeComb(N, M);
+		
+		
+		System.out.println(result);
+		br.close();
+	}
 }

57af22940c595a72e4424bee82822af201408042
zae-hyeong <p0138@naver.com>
2025-07-28 22:14:13 +0900

    [baekjoon, fail] s2_1260

--- baekjoon/s2_1260/Main.java
+++ baekjoon/s2_1260/Main.java
@@ -4,45 +4,105 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[][] arr;
-	static int[] temp;
-	static ArrayList<int[]> comb = new ArrayList<>();
-	static int result = 0;
+	static int N, M, V; //정점 수, 간선 수, 시작 정점
+	static Graph graph;
 	
-	public static void makeComb(int[] targetArr, int cnt, int start) {
-		if(cnt == M) {
-			comb.add(Arrays.copyOf(temp, M));
-			return;
+	public static ArrayList<Integer> dfsFrom(int startNode) {
+		ArrayList<Integer> result = new ArrayList<>();
+		boolean[] visited = new boolean[N + 1]; 
+		
+		ArrayDeque<Integer> stack = new ArrayDeque<>();
+		stack.add(startNode);
+		visited[startNode] = true;
+		
+		result.add(startNode);
+		
+		int node;
+		
+		while(!stack.isEmpty()) {
+			node = stack.pop();
+			
+			for(int i = 0; i < graph.hm.get(node).size(); i++) {
+				if(visited[graph.hm.get(node).get(i)]) continue;
+				
+				visited[graph.hm.get(node).get(i)] = true;
+				result.add(graph.hm.get(node).get(i));
+				stack.add(graph.hm.get(node).get(i));
+				break;
+			}
 		}
 		
-		for (int i = start; i < N; i++) {
-			temp[cnt] = targetArr[i];
-			makeComb(int[] targetArr, cnt + 1, i + 1);
-		}
-		return;
+		return result;
 	}
-
+	
+	public static ArrayList<Integer> bfsFrom(int startNode) {
+		ArrayList<Integer> result = new ArrayList<>();
+		boolean[] visited = new boolean[N + 1]; 
+		
+		ArrayDeque<Integer> queue = new ArrayDeque<>();
+		queue.add(startNode);
+		visited[startNode] = true;
+		
+		result.add(startNode);
+		
+		int node;
+		
+		while(!queue.isEmpty()) {
+			node = queue.poll();
+			
+			for(int i = 0; i < graph.hm.get(node).size(); i++) {
+				if(visited[graph.hm.get(node).get(i)]) continue;
+				
+				visited[graph.hm.get(node).get(i)] = true;
+				result.add(graph.hm.get(node).get(i));
+				queue.add(graph.hm.get(node).get(i));
+			}
+		}
+		
+		return result;
+	}
+	
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("res/test.txt"));
+		System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
 		
-		for(int i = 0; i < N; i++) {
+		graph = new Graph(N);
+		
+		int a, b;
+		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			
+			graph.connect(a, b);
 		}
 		
-		int[][] comb = makeComb(N, M);
+		graph.sort();
+//		graph.print();
 		
+		ArrayList<Integer> dfsResult = dfsFrom(V);
+		ArrayList<Integer> bfsResult = bfsFrom(V); 
 		
-		System.out.println(result);
+		for(int i = 0; i < dfsResult.size(); i++) {
+			sb.append(dfsResult.get(i));
+			if(i < dfsResult.size() - 1)
+				sb.append(" ");
+		}
+		sb.append("\n");
+		for(int i = 0; i < bfsResult.size(); i++) {
+			sb.append(bfsResult.get(i));
+			if(i < bfsResult.size() - 1)
+				sb.append(" ");
+		}
+		
+		System.out.println(sb.toString());
 		br.close();
 	}
 }

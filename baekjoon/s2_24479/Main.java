bd36c7fcb2c2a2e24b0da1f12326818a9cb1e9e8
zae-hyeong <p0138@naver.com>
2025-07-31 11:49:06 +0900

    [bj] s2_24479

--- baekjoon/s2_24479/Main.java
+++ baekjoon/s2_24479/Main.java
@@ -1,20 +1,23 @@
 package s2_24479;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M, R;
 	static int[] visited;
 	static Graph g;
-	
-	public static void dfs(int V, int cnt) {
-		visited[V] = cnt;
-		
-		for(int nextNode: g.hm.get(V)) {
-			if(visited[nextNode] > 0) continue;
-			
-			dfs(nextNode, cnt + 1);
+
+	static int cnt = 1;
+
+	public static void dfs(int V) {
+		visited[V] = cnt++;
+
+		for (int nextNode : g.hm.get(V)) {
+			if (visited[nextNode] == 0)
+				dfs(nextNode);
 		}
 	}
 
@@ -23,15 +26,15 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		
+
 		visited = new int[N + 1];
 		g = new Graph(N);
-		
+
 		int a, b;
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -39,16 +42,16 @@ public class Main {
 			b = Integer.parseInt(st.nextToken());
 			g.connect(a, b);
 		}
-		
+
 		g.sort();
-		
-		dfs(R, 1);
-		
+
+		dfs(R);
+
 		for (int i = 1; i < visited.length; i++) {
 			sb.append(visited[i]).append("\n");
 		}
-		
-		System.out.println(sb.toString());
+
+		System.out.print(sb.toString());
 
 		br.close();
 	}

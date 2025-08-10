8f4d19e5cd0b3430d02a3ba762af1d10b0fcf9fb
zae-hyeong <p0138@naver.com>
2025-08-10 13:14:49 +0900

    bj: s2_2477

--- solving/silver/s2_2477/Main.java
+++ solving/silver/s2_2477/Main.java
@@ -5,47 +5,40 @@ import java.util.*;
 
 public class Main {
 	static int N;
-	static List<Integer>[] arr;
+	static int[][] inputArr;
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("./solving/silver/s2_2477/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
-		arr = new ArrayList[4];
-		for (int i = 0; i < 4; i++)
-			arr[i] = new ArrayList<>();
+		inputArr = new int[6][2];
 
 		StringTokenizer st;
 		int num, dis;
+		int[] size = new int[5];
 		for (int i = 0; i < 6; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			num = Integer.parseInt(st.nextToken());
 			dis = Integer.parseInt(st.nextToken());
-			arr[num - 1].add(dis);
+			inputArr[i] = new int[] {num, dis};
+			size[num]++;
 		}
 
-		int[] order = { 0, 2, 1, 3 };
-		int bigSize = 1, smallSize = 1, oo = 1;
-		for (int i = 0; i < 4; i++) {
-			System.out.println(arr[order[i]].toString());
-			if (arr[order[i]].size() == 1) {
-				bigSize *= arr[order[i]].get(0);
-
-				if (i > 0 && arr[order[i - 1]].size() == 2) {
-					smallSize *= arr[order[i - 1]].get(0);
-				}
-				if (i == 0 && arr[order[3]].size() == 2) {
-					smallSize *= arr[order[3]].get(0);
-				}
-
-				if (i < 3 && arr[order[i + 1]].size() == 2) {
-					smallSize *= arr[order[i + 1]].get(1);
-				}
-				if (i == 3 && arr[order[0]].size() == 2) {
-					smallSize *= arr[order[0]].get(1);
-				}
+		int bigSize = 1, smallSize = 1;
+		int a, b;
+		for (int i = 0; i < 6; i++) {
+			if(size[inputArr[i][0]] == 1) {
+				bigSize *= inputArr[i][1];
+				
+				if(i ==0) a = inputArr[5][1];
+				else a = inputArr[i-1][1];
+				
+				if( i == 5) b = inputArr[0][1];
+				else b=inputArr[i+1][1];
+				
+				smallSize *= (Math.abs(a - b));
 			}
 		}
 

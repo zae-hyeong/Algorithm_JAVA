0883147d766f09538fd106aa6c8bbe599b796f77
zae-hyeong <p0138@naver.com>
2025-08-11 20:59:14 +0900

    bj: g2_3109

--- solving/gold/g2_3109/Main.java
+++ solving/gold/g2_3109/Main.java
@@ -4,60 +4,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int R, C;
-    static char[][] arr;
-    static boolean[][] visited;
-    static int count;
-    static int[] dy = { -1, 0, 1 };
+	static int R, C;
+	static char[][] arr;
+	static int count;
+	static int[] dy = { -1, 0, 1 };
 
-    static boolean isValid(int y, int x) {
-        return 0 <= y && y < R && 0 <= x && x < C && !visited[y][x] && arr[y][x] == '.';
-    }
+	static boolean isValid(int y, int x) {
+		return 0 <= y && y < R && 0 <= x && x < C;
+	}
 
-    static boolean dfsFrom(int startY, int startX) {
-        visited[startY][startX] = true;
-//        System.out.println("====" + startY + ":" + startX + "====");
+	static boolean dfsFrom(int startY, int startX) {
+		if (startX == C - 1) {
+			count++;
+			return true;
+		}
 
-        if (startX == R - 1) {
-            count++;
-            return true;
-        }
+		for (int d : dy) {
+			int ny = startY + d;
+			int nx = startX + 1;
+			
+			if (!isValid(ny, nx))
+				continue;
+			if (arr[ny][nx] == '.') {
+				arr[ny][nx] = 'x';
+				if (dfsFrom(ny, nx)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-        for (int d : dy) {
-//            System.out.println((startY + d) + "::" + (startX + 1) +"::"+isValid(startY + 1, startX + d));
-            if (isValid(startY + d, startX + 1))
-                if (dfsFrom(startY + d, startX + 1))
-                    return true;
-        }
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("./solving/gold/g2_3109/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        visited[startY][startX] = false;
-        return false;
-    }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws Exception {
-    	System.setIn(new FileInputStream("./solving/gold/g2_3109/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		arr = new char[R][C];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < R; i++)
+			arr[i] = br.readLine().toCharArray();
 
-        arr = new char[R][C];
-        visited = new boolean[R][C];
+		int count = 0;
+		for (int i = 0; i < R; i++) {
+			if (dfsFrom(i, 0))
+				count++;
+		}
 
-        for (int i = 0; i < R; i++) arr[i] = br.readLine().toCharArray();
-
-//        for (char[] c : arr) System.out.println(Arrays.toString(c));
-
-        int count = 0;
-        for (int i = 0; i < R; i++) {
-            if (dfsFrom(i, 0))
-                count++;
-//            for (boolean[] b : visited) System.out.println(Arrays.toString(b));
-//            System.out.println("----------------------");
-        }
-
-        System.out.println(count);
-        br.close();
-    }
+		System.out.println(count);
+		br.close();
+	}
 }

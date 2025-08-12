79005654e67103b252cfaccbceb3f13d267379ed
zae-hyeong <p0138@naver.com>
2025-08-12 11:03:59 +0900

    bj: s2_11279

--- baekjoon/silver/s2_11279/Main.java
+++ baekjoon/silver/s2_11279/Main.java
@@ -3,29 +3,96 @@ package silver.s2_11279;
 import java.io.*;
 import java.util.*;
 
+class MaxHeap {
+	private ArrayList<Integer> arr;
+
+	MaxHeap() {
+		this.arr = new ArrayList<>();
+	}
+
+	void add(int v) {
+		arr.add(v);
+		bubbleUp();
+		return;
+	}
+
+	int poll() {
+		if (this.arr.isEmpty())
+			return 0;
+
+		swap(0, arr.size() - 1);
+		int returnVal = arr.get(arr.size() - 1);
+		arr.remove(arr.size() - 1);
+
+		bubbleDown();
+
+		return returnVal;
+	}
+
+	private void bubbleUp() {
+		int child = arr.size() - 1;
+		int parent = (child - 1) / 2;
+
+		while (arr.get(parent) < arr.get(child)) {
+			swap(parent, child);
+
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+	}
+
+	private void bubbleDown() {
+
+		int parent = 0, maxIdx = 0;
+		int leftChild = parent * 2 + 1;
+		int rightChild = parent * 2 + 2;
+
+		while (true) {
+			maxIdx = parent;
+
+			if (leftChild < arr.size() && this.arr.get(maxIdx) < this.arr.get(leftChild))
+				maxIdx = leftChild;
+
+			if (rightChild < arr.size() && this.arr.get(maxIdx) < this.arr.get(rightChild))
+				maxIdx = rightChild;
+
+			if (parent == maxIdx) break;
+
+			swap(maxIdx, parent);
+			parent = maxIdx;
+			leftChild = parent * 2 + 1;
+			rightChild = parent * 2 + 2;
+		}
+	}
+
+	private void swap(int a, int b) {
+		int valA = this.arr.get(a);
+		int valB = this.arr.get(b);
+		this.arr.set(a, valB);
+		this.arr.set(b, valA);
+	}
+}
+
 public class Main {
 	static int N;
-	static PriorityQueue<Integer> ad;
+	static MaxHeap mh;
 
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("./solving/silver/s2_11279/input.txt"));
+		System.setIn(new FileInputStream("./baekjoon/silver/s2_11279/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
 		N = Integer.parseInt(br.readLine());
-		ad = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
+		mh = new MaxHeap();
 
 		int num;
 		for (int i = 0; i < N; i++) {
 			num = Integer.parseInt(br.readLine());
 
-			if (num == 0) {
-				if (!ad.isEmpty())
-					sb.append(ad.poll()).append("\n");
-				else
-					sb.append(0).append("\n");
-			} else
-				ad.offer(num);
+			if (num == 0)
+				sb.append(mh.poll()).append("\n");
+			else
+				mh.add(num);
 		}
 
 		System.out.println(sb.toString());

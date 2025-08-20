a76cb6e740f45761df68b0036278c548fe78b559
zae-hyeong <p0138@naver.com>
2025-08-20 09:46:20 +0900

    add default

---
+++ solving/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	static int result;
+	
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream(""));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		final int TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			result = 0;
+			
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+}

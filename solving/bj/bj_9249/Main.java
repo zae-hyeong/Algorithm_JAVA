2dd977cae29eef0b87094d5f4c237a04d91be49d
zae-hyeong <p0138@naver.com>
2025-09-12 11:33:15 +0900

    bj, memory fail: 9249

--- solving/bj/bj_9249/Main.java
+++ solving/bj/bj_9249/Main.java
@@ -4,8 +4,19 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static char[] str1, str2;
+	static char[] str1, str2, cache;
 	static String[][] dp;
+
+	static String lcs(int i, int j) {
+		if (i == 0 || j == 0 || str1[i - 1] != str2[j - 1])
+			return "";
+
+		if (dp[i - 1][j - 1] != null) {
+			return dp[i - 1][j - 1]+ str1[i - 1];
+		}
+
+		return dp[i][j] = lcs(i - 1, j - 1) + str1[i - 1];
+	}
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("./solving/bj/bj_9249/input.txt"));
@@ -15,29 +26,24 @@ public class Main {
 		str2 = br.readLine().toCharArray();
 
 		dp = new String[str1.length + 1][str2.length + 1];
-		for (int i = 0; i <= str1.length; i++) {
-			for (int j = 0; j <= str2.length; j++) {
-				dp[i][j] = "";
-			}
-		}
 
-		int maxSize = 0;
-		String result = null;
+		String maxStr = "";
+
 		for (int i = 1; i <= str1.length; i++) {
 			for (int j = 1; j <= str2.length; j++) {
 				if (str1[i - 1] == str2[j - 1]) {
-					dp[i][j] = dp[i - 1][j - 1] + str1[i - 1];
-					if(maxSize < dp[i][j].length()) {
-						maxSize = dp[i][j].length();
-						result = dp[i][j];
+					String s = lcs(i, j);
+
+					if (maxStr.length() < s.length()) {
+						maxStr = s;
 					}
 				}
 			}
 		}
 
 //		for (String[] d : dp) System.out.println(Arrays.toString(d));
-		System.out.println(maxSize);
-		System.out.println(result);
+
+		System.out.println(maxStr);
 		br.close();
 	}
 }

4c9cc43a7c038d56e34ad266fac0a136f030bebb
zae-hyeong <p0138@naver.com>
2025-09-16 12:11:54 +0900

    bj: b2_9253

--- baekjoon/bj/bj_9253/Main.java
+++ baekjoon/bj/bj_9253/Main.java
@@ -4,19 +4,19 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static String str1, str2, cmpStr;
+	static char[] str1, str2, cmpStr;
 
-	static int[] makeTable(String str) {
-		int n = str.length();
+	static int[] makeTable(char[] str) {
+		int n = str.length;
 		int[] arr = new int[n];
 
 		int idx = 0;
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i < n; i++) {
 
-			while (idx > 0 && str.charAt(i) != str.charAt(idx))
+			while (idx > 0 && str[i] != str[idx])
 				idx = arr[idx - 1];
 
-			if (str.charAt(i) == str.charAt(idx)) {
+			if (str[i] == str[idx]) {
 				idx++;
 				arr[i] = idx;
 			}
@@ -25,18 +25,18 @@ public class Main {
 		return arr;
 	}
 
-	static boolean KMP(String str, String cmp, int[] table) {
-		int n = str.length();
+	static boolean KMP(char[] str, char[] cmp, int[] table) {
+		int n = str.length;
 
 		int idx = 0;
 		for (int i = 0; i < n; i++) {
-			while (idx > 0 && str.charAt(i) != cmp.charAt(idx))
+			while (idx > 0 && str[i] != cmp[idx])
 				idx = table[idx - 1];
 
-			if (str.charAt(i) == cmp.charAt(idx)) {
-				if (idx == cmp.length() - 1)
+			if (str[i] == cmp[idx]) {
+				if (idx == cmp.length - 1)
 					return true;
-				
+
 				idx++;
 			}
 		}
@@ -45,12 +45,12 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("./solving/bj/bj_9253/input.txt"));
+//		System.setIn(new FileInputStream("./solving/bj/bj_9253/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		str1 = br.readLine();
-		str2 = br.readLine();
-		cmpStr = br.readLine();
+		str1 = br.readLine().toCharArray();
+		str2 = br.readLine().toCharArray();
+		cmpStr = br.readLine().toCharArray();
 
 		int[] table = makeTable(cmpStr);
 		boolean v1 = KMP(str1, cmpStr, table);

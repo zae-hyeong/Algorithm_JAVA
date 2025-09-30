a963db3386f998b0c751f7a941c5aaef87178060
zae-hyeong <p0138@naver.com>
2025-09-30 22:54:35 +0900

    bj: 1484, 2470

---
+++ baekjoon/gold/g5_1484_다이어트/Main.java
@@ -0,0 +1,36 @@
+package gold.g5_1484;
+
+import java.util.*;
+import java.io.*;
+
+// 예시) input : 15
+// output : 4, 8 : 64-49, 16-1
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("./solving/gold/g5_1484/input.txt"));
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		int front = 1, rear = 2, count = 0;
+		double num;
+
+		while (front < rear) {
+//			System.out.println(front + " : " + rear);
+			num = Math.pow(rear, 2) - Math.pow(front, 2);
+			if (num < N)
+				rear++;
+			else if (num > N)
+				front++;
+			else {
+				sb.append(rear).append("\n");
+				count++;
+				front++;
+			}
+		}
+
+		System.out.println(count == 0 ? -1 : sb.toString());
+		sc.close();
+	}
+}
---
+++ baekjoon/gold/g5_2470_두용액/Main.java
@@ -0,0 +1,43 @@
+package gold.g5_2470;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N;
+	static int[] arr;
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("./solving/gold/g5_2470/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+
+		br.close();
+		int num1 = 0, num2 = 0, min = Integer.MAX_VALUE;
+
+		Arrays.sort(arr);
+		int front = 0, rear = N - 1, sum;
+		while (front < rear) {
+			sum = arr[front] + arr[rear];
+			
+			if (min > Math.abs(sum)) {
+				min = Math.abs(sum);
+				
+				num1 = arr[front];
+				num2 = arr[rear];
+				
+				if(min == 0) break;
+			}
+			
+			if (sum < 0) front++;
+			else rear--;
+		}
+
+		System.out.println(num1 + " " + num2);
+	}
+}

1381f435afd0488eec5c1b4cecbae8c6185a76f2
zae_hyeong <p0138@naver.com>
2025-11-10 15:02:22 +0900

    bj: g3_11066 합치기

--- solving/gold/g3_11066_파일합치기/Main.java
+++ solving/gold/g3_11066_파일합치기/Main.java
@@ -6,58 +6,41 @@ import java.io.*;
 public class Main {
 	static int N;
 	static int[] arr, sum;
-
-	static int split(int start, int end) {
-		int leftSum, rightSum;
-		int minDiff = Integer.MAX_VALUE, splitPoint = -1;
-		
-		if(start >= end) return 0;
-		
-		int cost = 0;
-		for(int i = start; i < end; i++) {
-			leftSum = sum[i] - sum[start - 1];
-			rightSum = sum[end] - sum[i];
-			
-			if(Math.abs(leftSum - rightSum) < minDiff) {
-				splitPoint = i;
-				minDiff = Math.abs(leftSum - rightSum);
-				cost = leftSum + rightSum;
-			}
-		}
-		
-		leftSum = split(start, splitPoint);
-		rightSum = split(splitPoint + 1, end);
-		
-		cost = cost + leftSum + rightSum;
-		
-		System.out.println("------------------------------");
-		System.out.println(String.format("start: %d, splitPoint : %d, end : %d", start, splitPoint, end));
-		System.out.println(String.format("leftSum : %d, rightSum : %d, cost: %d", leftSum, rightSum, cost));
-		
-		return cost;
-	}
+	static int[][] dp;
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("./solving/gold/g3_11066_파일합치기/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-//		
 		final int TC = Integer.parseInt(br.readLine());
-		for(int tc = 0; tc < TC; tc++) {
+		for (int tc = 0; tc < TC; tc++) {
 			N = Integer.parseInt(br.readLine());
-			
-			arr = new int[N+1];
-			sum = new int[N+1];
-			
+
+			arr = new int[N + 1];
+			sum = new int[N + 1];
+			dp = new int[N + 1][N + 1];
+
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int i = 1; i <= N; i++) {
+			for (int i = 1; i <= N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
-				sum[i] = sum[i-1] + arr[i];
+				sum[i] = sum[i - 1] + arr[i];
 			}
 			
-			System.out.println(split(1, N));
+			for(int range = 1; range <= N; range++) {
+				for(int i = 1; i <= N - range; i++) {
+					dp[i][i + range] = Integer.MAX_VALUE;
+					
+					for(int k = i; k < i + range; k++) {
+						dp[i][i + range] = Math.min(
+								dp[i][i + range], 
+								dp[i][k] + dp[k + 1][i + range] + sum[i+range] - sum[i - 1]);
+					}
+				}
+			}
+			
+			System.out.println(dp[1][N]);
 		}
-		
+
 		br.close();
 	}
 }

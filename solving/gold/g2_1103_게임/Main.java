523651041c29bf68319ef688f3b1f9ab42d33cfb
zae-hyeong <p0138@naver.com>
2025-12-01 10:33:52 +0900

    bj, timeout: g2_1103_게임

---
+++ solving/gold/g2_1103_게임/Main.java
@@ -0,0 +1,72 @@
+package gold.g2_1103_게임;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int W, H;
+	static int[][] map;
+	static boolean[][] v;
+	
+	static int max;
+	
+	static int[] dy = {1, 0, -1, 0};
+	static int[] dx = {0, 1, 0, -1};
+	
+	static int ny, nx;
+	static void dfs(int y, int x, int depth) {
+//		System.out.format("y : %d, x : %d, map[y][x] : %d, depth : %d\n", y, x, map[y][x], depth);
+		if(v[y][x]) {
+			System.out.println(-1);
+			System.exit(0);
+		}
+		
+		v[y][x] = true;
+		max = Math.max(max, depth);
+		
+		if(map[y][x] == -1) {
+			v[y][x] = false;
+			return;
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			ny = y + dy[i] * map[y][x];
+			nx = x + dx[i] * map[y][x];
+			
+			if(0 <= ny && ny < H && 0 <= nx && nx < W && map[ny][nx] > -1) {
+				dfs(ny, nx, depth + 1);
+			}
+		}
+		
+		v[y][x] = false;
+	}
+	
+	public static void main(String[] args) throws Exception {
+//		System.setIn(new FileInputStream("./solving/gold/g2_1103_게임/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		H = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
+		map = new int[H][W];
+		v = new boolean[H][W];
+		
+		String tmp;
+		char c;
+		for (int i = 0; i < H; i++) {
+			tmp = br.readLine();
+			for (int j = 0; j < W; j++) {
+				c = tmp.charAt(j);
+				map[i][j] = c == 'H' ? -1 : (int) c - '0';
+			}
+		}
+		
+//		for(int[] m: map) System.out.println(Arrays.toString(m));
+		max = 0;
+		dfs(0, 0, 1);
+		
+		System.out.println(max);
+
+		br.close();
+	}
+}
